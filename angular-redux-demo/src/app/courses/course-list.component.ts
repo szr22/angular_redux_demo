dfa96189139f5d5ee4d00e9e3288de9777bf61fc
szr22 <shizhenrong1987@hotmail.com>
2019-06-15 04:03:01 -0400

    Implementing a Basic Integration of Redux and Angular 2

--- angular-redux-demo/src/app/courses/course-list.component.ts
+++ angular-redux-demo/src/app/courses/course-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CourseService } from './course.service';
 import { Course } from './course';
 import { FilterTextComponent, FilterService } from '../blocks/filter-text';
+import { store } from '../store';
 
 @Component({
   selector: 'app-course-list',
@@ -20,15 +21,17 @@ export class CourseListComponent implements OnInit {
     this.filteredCourses = this._filterService.filter(searchText, ['id', 'name', 'topic'], this.courses);
   }
 
-  getCourses() {
-    this._courseService.getCourses()
-      .subscribe(courses => {
-        this.courses = this.filteredCourses = courses;
-      });
+  updateFromState() {
+    const allState = store.getState();
+    this.courses = allState.courses;
+    this.filteredCourses = allState.courses;
   }
 
   ngOnInit() {
-    this.getCourses();
+    this.updateFromState();
+    store.subscribe(() => {
+      this.updateFromState();
+    });
     componentHandler.upgradeDom();
   }
 }
---
+++ angular-redux-demo/src/app/store/reducer.ts
@@ -0,0 +1,27 @@
+import { Course } from '../courses/course';
+import { IAppState } from './IAppState';
+import { ftruncate } from 'fs';
+
+const initialState: IAppState = {
+    courses: [
+        {
+          "id": 1,
+          "name": "Building Apps with React",
+          "topic": "ReactJS"
+        },
+        {
+          "id": 2,
+          "name": "Building Apps with Angular",
+          "topic": "AngularJS"
+        },
+        {
+          "id":3,
+          "name": "Building Apps with Angular and Redux",
+          "topic": "Angular and Redux"
+        }
+    ]
+};
+
+export function reducer(state = initialState, action){
+    return state;
+}
