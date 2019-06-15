c3daf4acfab518dfcd2fa0e59221ee990522e4b2
szr22 <shizhenrong1987@hotmail.com>
2019-06-15 11:35:21 -0400

    Handling User Input Through a Redux Action

--- angular-redux-demo/src/app/store/reducer.ts
+++ angular-redux-demo/src/app/store/reducer.ts
@@ -1,27 +1,43 @@
 import { Course } from '../courses/course';
 import { IAppState } from './IAppState';
-import { ftruncate } from 'fs';
+import { FILTER_COURSES } from './action';
+
+
+const courses = [
+  {
+    "id": 1,
+    "name": "Building Apps with React",
+    "topic": "ReactJS"
+  },
+  {
+    "id": 2,
+    "name": "Building Apps with Angular",
+    "topic": "AngularJS"
+  },
+  {
+    "id":3,
+    "name": "Building Apps with Angular and Redux",
+    "topic": "Angular and Redux"
+  }
+]
 
 const initialState: IAppState = {
-    courses: [
-        {
-          "id": 1,
-          "name": "Building Apps with React",
-          "topic": "ReactJS"
-        },
-        {
-          "id": 2,
-          "name": "Building Apps with Angular",
-          "topic": "AngularJS"
-        },
-        {
-          "id":3,
-          "name": "Building Apps with Angular and Redux",
-          "topic": "Angular and Redux"
-        }
-    ]
+  courses,
+  filteredCourses: courses,
 };
 
+function filterCourses(state, action) : IAppState {
+  return Object.assign({}, state, {
+    filteredCourses: state.courses.filter( c => c.name.toLowerCase().indexOf
+    (action.searchText.toLowerCase()) > -1),
+  })
+}
+
 export function reducer(state = initialState, action){
-    return state;
+  switch(action.type) {
+    case FILTER_COURSES:
+      return filterCourses(state, action);
+    default:
+      return state;
+  }
 }
